56c0fa8e4dc4099ac70f8109594b43bc509e46a8
gurunaik111 <gurunaik111@gmail.com>
2017-08-12 23:00:46 +0530

    working bottom bar.. activity not in sequence. need to work

--- app/src/main/java/com/guru/parentapp2/MainActivity.java
+++ app/src/main/java/com/guru/parentapp2/MainActivity.java
@@ -1,15 +1,24 @@
 package com.guru.parentapp2;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+
+    //..for logging..
+    private static final String TAG = "MainActivity";
+
+    //..The request code is supposed to be unique?..
+    public static final int MY_REQUEST_CODE = 123;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -40,6 +49,37 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void pushFxn()
+    {
+        Intent intent =
+                new Intent(
+                        this,
+                        SignupActivity.class );
+
+        startActivityForResult( intent, MY_REQUEST_CODE );
+    }
+
+    protected void onActivityResult(
+            int requestCode,
+            int resultCode,
+            Intent pData)
+    {
+        if ( requestCode == MY_REQUEST_CODE )
+        {
+            if (resultCode == Activity.RESULT_OK )
+            {
+                final String zData = pData.getExtras().getString( SignupActivity.EXTRA_STRING_NAME );
+
+                //..do something with our retrieved value..
+
+                Log.v( TAG, "Retrieved Value zData is "+zData );
+                //..logcats "Retrieved Value zData is returnValueAsString"
+
+            }
+        }
+
     }
 
 }
--- app/src/main/java/com/guru/parentapp2/SignupActivity.java
+++ app/src/main/java/com/guru/parentapp2/SignupActivity.java
@@ -1,14 +1,42 @@
 package com.guru.parentapp2;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
 /**
  * Created by GuRu on 12/08/2017.
  */
 
-public class SignupActivity {
+public class SignupActivity extends Activity{
+
+    public static final String EXTRA_STRING_NAME="extraStringName";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_signup);
 
 
+    }
 
+    public void onSubmitButtonClick(View view) {
+       // String c= R.id.class;
 
+        Intent iData = new Intent();
+        iData.putExtra(
+                EXTRA_STRING_NAME,
+                "returnValueAsString" );
 
+        setResult(
+                android.app.Activity.RESULT_OK,
+                iData );
 
+        //..returns us to the parent "MyMainActivity"..
+        finish();
+
+    }
 }
